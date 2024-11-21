4be524592d0b34dcc473936fcd8aa2acf335028f
Simone Paoletti <simonepaoletti17@gmail.com>
2024-11-21 17:13:18 +0100

    quinto commit

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ st.altair_chart(
     pie_chart,
     use_container_width=True
 )
-
+###########################################################################################
 source = alt.topo_feature(data.world_110m.url, 'countries')
 input_dropdown = alt.binding_select(options=[
     "equalEarth",
@@ -51,28 +51,9 @@ input_dropdown = alt.binding_select(options=[
 
 param_projection = alt.param(value="equalEarth", bind=input_dropdown)
 
-#from altair import expr
-#
-#countries = alt.topo_feature(data.world_110m.url, 'countries')
-#
-## Rinominare le colonne per corrispondere, se necessario (es: country -> name)
-#data_07 = data_07.rename({"country": "name"})  # Adatta il nome se serve
-#print(data_07)
-## Unire i dati geografici con il dataset dei paesi
-#merged_data = (
-#    alt.Chart(countries)
-#    .transform_lookup(
-#        lookup="properties.name",  # Nome del campo geografico
-#        from_=alt.LookupData(data_07.to_pandas(), "name", ["lifeExp"])  # Dati da unire
-#    )
-#)
-#
-
 df_us_unemp = data.unemployment()
 print(df_us_unemp)
 
-#countries = gpd.read_file(data.world_110m.url, driver='TopoJSON', layer='countries')
-#print(countries)
 map = (
     alt.Chart(source, width=500, height=300)
     .mark_geoshape(fill='lightgray', stroke='gray')
@@ -86,18 +67,5 @@ map = (
 
 st.altair_chart(
     map,
-    use_container_width=True
-)
-
-print(data_07.select("lifeExp"))
-
-gdf_world = gpd.read_file(data.world_110m.url, driver="TopoJSON")
-
-world = alt.Chart(gdf_world).mark_geoshape(
-    fill="mintcream", stroke="black", strokeWidth=0.35
-)
-
-st.altair_chart(
-    world,
     use_container_width=True
 )
