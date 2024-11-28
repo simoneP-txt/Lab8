6e27fb20ebc1fc223b3e1e72047f2a4bb14d3b43
Simone Paoletti <simonepaoletti17@gmail.com>
2024-11-28 19:10:47 +0100

    settimo commit

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ url = "https://www.dei.unipd.it/~ceccarello/data/gapminder.csv"
 
 data1 = pl.read_csv(url)
 
-st.dataframe(data1)
-
 ts = (
     alt.Chart(data1)
     .mark_line()
@@ -27,6 +25,7 @@ st.altair_chart(
 )
 
 data_07 = data1.filter(pl.col("year") == 2007)
+st.dataframe(data_07)
 
 pie_chart = (
     alt.Chart(data_07)
@@ -46,68 +45,114 @@ st.altair_chart(
     use_container_width=True
 )
 
-base_pie= (
-    alt.Chart(data_07)
-    .mark_arc(cornerRadius=5)
+@st.cache_data
+def get_data(url):
+    return pl.read_csv(url)
+
+data = get_data(url)
+data2007 = data.filter(pl.col("year") == 2007)
+
+total_pop_2007 = data2007.select(pl.col("pop").sum())
+
+
+base_chart = (
+    alt.Chart(data)
     .transform_aggregate(
         pop = "sum(pop)",
         groupby = ["continent"]
     )
+)
+
+base_pie = (
+    base_chart
+    .mark_arc(
+        radius = 80,
+        radius2 = 120,
+        cornerRadius = 20
+    )
     .encode(
-        alt.Theta("pop", aggregate = "sum"),
+        alt.Theta("pop"),
         alt.Color("continent")
     )
 )
 
 text_pie = (
-    base_pie
-    .mark_text()
+    base_chart
+    .mark_text(
+        radius = 150,
+        size=15
+    )
+    .transform_calculate(
+        label = "round(datum.pop / 1000000) + ' M'"
+    )
     .encode(
-        alt.Text("pop")
+        alt.Text("label:N"),
+        alt.Theta("pop", stack=True),
+        alt.Order("continent")
     )
 )
-###########################################################################################
-source = alt.topo_feature(data.world_110m.url, 'countries')
-input_dropdown = alt.binding_select(options=[
-    "equalEarth",
-    "naturalEarth1",
-    "orthographic"
-], name='Projection ')
 
-param_projection = alt.param(value="equalEarth", bind=input_dropdown)
-
-#map = (
-#    alt.Chart(source, width=500, height=300)
-#    .mark_geoshape(fill='lightgray', stroke='gray') #contorni grigi
-#    #.transform_lookup()
-#    .project(type=alt.expr(param_projection.name))
-#    .add_params(param_projection)
-#    .encode(
-#        #color=data_07["lifeExp"]
-#    )
-#)
-
-map = (
-    alt.Chart(source, width=500, height=300)
-    .mark_geoshape()
-    .transform_lookup(
-        lookup="id",  # Proprietà del TopoJSON da usare come chiave
-        from_=alt.LookupData(data_07.to_pandas(), "id", ["lifeExp"])  # Dati da unire
+text_total = (
+    base_chart
+    .mark_text(
+        radius = 0,
+        size=30
+    )
+    .transform_aggregate(
+        pop = "sum(pop)"
+    )
+    .transform_calculate(
+        label = "round(datum.pop / 1000000) + ' M'"
     )
     .encode(
-        #color=alt.Color("lifeExp:Q", scale=alt.Scale(scheme="viridis"), title="Life Expectancy"),
-        #tooltip=[
-        #    alt.Tooltip("id:N", title="Country ID"),
-        #    alt.Tooltip("lifeExp:Q", title="Life Expectancy")
-        #]
+        alt.Text("label:N")
     )
-    .project(type=alt.expr(param_projection.name))
-    .add_params(param_projection)
+)
+
+chart = (
+    base_pie + text_pie + text_total
+).properties(
+    height = 30,
+    width = 30
+).facet(
+    "year", columns=2
 )
 
 st.altair_chart(
-    map,
+    chart,
     use_container_width=True
 )
+###########################################################################################
+#congo, dem congo, repubblica centro africana, sudan del sud
+data_07_new = data_07.with_columns(
+        pl.when(pl.col("country") == "United States")
+        .then("United States of America")
+        .otherwise(pl.col("country"))
+        .alias("country")
+    )
+print(data_07_new)
+
+map110 = (alt.Chart(alt.topo_feature("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json", 'countries'))
+         .mark_geoshape(stroke = "black").encode(
+    color=alt.Color('lifeExp:Q', scale=alt.Scale(scheme='greens')),
+    tooltip=['country:N', 'lifeExp:Q']  # tooltip per visualizzare informazioni
+    ).transform_lookup(
+    lookup='properties.name',  # chiave comune per unire (nel TopoJSON)
+    from_=alt.LookupData(data_07, 'country', ['lifeExp'])  # fonte dei dati
+    ).project(
+    type='mercator'  # proiezione geografica
+    ).properties(
+    width=800,
+    height=600,
+    title="Aspettativa di vita nel 2007, scala 1:110"
+    )
+)
+
+st.altair_chart(
+    map110,
+    use_container_width=True
+)
+
+
 
 ###########################################################################################################
--- test.py
+++ test.py
@@ -1,56 +1,8 @@
-import json
-from urllib.request import urlopen
-from vega_datasets import data
 import polars as pl
-import pandas as pd
-from icecream import ic
-import geopandas as gpd
-import altair as alt
-import streamlit as st
-
-gdf_us_counties = gpd.read_file(data.us_10m.url, driver='TopoJSON', layer='counties')
-df_us_unemp = data.unemployment()
-
-map = alt.Chart(gdf_us_counties).mark_geoshape().transform_lookup(
-    lookup='id',
-    from_=alt.LookupData(data=df_us_unemp, key='id', fields=['rate'])
-).encode(
-    alt.Color('rate:Q')
-).project(
-    type='albersUsa'
-)
-
-st.altair_chart(
-    map
-)
-#############################################################
-url = data.world_110m.url
-with urlopen(url) as response:
-    topojson = json.load(response)
-
-# Estrarre gli id e i nomi dai paesi
-countries = topojson["objects"]["countries"]["geometries"]
-
-# Analizza la struttura per identificare le chiavi disponibili
-for country in countries[:5]:  # Guarda solo i primi 5 paesi per non sovraccaricare l'output
-    print(country)
 
 url = "https://www.dei.unipd.it/~ceccarello/data/gapminder.csv"
 
 data1 = pl.read_csv(url)
 data_07 = data1.filter(pl.col("year") == 2007)
-
-#id_to_country = {
-#    4: "Afghanistan",
-#    24: "Angola",
-#    8: "Albania",
-#    784: "United Arab Emirates",
-#    32: "Argentina",
-#}
-#
-#country_to_id = {v: k for k, v in id_to_country.items()}
-#
-#data_07_1 = data_07.with_columns(
-#    pl.col("country").map_elements(country_to_id).alias("id")
-#)
-#
+print(data_07)
+data_07.write_csv("data_07.csv")
